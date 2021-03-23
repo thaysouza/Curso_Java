8bac1490202bc26b406d268714955ec8d1d78dfd
unknown <tayanesilvasouzas@gmail.com>
2021-03-22 22:43:35 -0300

    atividade java

---
+++ src/Exercicio1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("Informe um numero ");
+        num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println(num + " é negatio");
+        } else {
+            System.out.println(num + " é positivo");
+        }
+
+        sc.close();
+
+    }
+}
