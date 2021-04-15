f93e0275faf389403331eaa1714b24ac0500424a
thaysouza <tayanesilvasouzas@gmail.com>
2021-04-14 23:05:08 -0300

    aula1-novotec

---
+++ curso_java_novotec/src/OperadoresMatematica/Doces.java
@@ -0,0 +1,21 @@
+
+package OperadoresMatematica;
+
+
+public class Doces {
+    
+    public static void main(String[] args){
+        double bombom, bala, paçoca,doceDeLeite;
+        
+       bombom = 5 * 2.50;
+       paçoca = 3 * 1.00;
+       bala =  20 * 0.25;
+       doceDeLeite = 8 * 0.60;
+               
+       double Total = bombom + paçoca + bala + doceDeLeite;
+ 
+    System.out.println("O valor total é: " + Total);
+ 
+    }
+    
+}
